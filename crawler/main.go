d1aef5c04555c871b32e8f8471425c70b9bec4f7
Mrwenshuai <1154355867@qq.com>
2019-11-17 17:56:19 +0800

    初步拉取并打印指定信息

--- crawler/main.go
+++ crawler/main.go
@@ -1,42 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"net/http"
-	"regexp"
+	"gowork/crawler/engine"
+	"gowork/crawler/zhenai/parser"
 )
 
 func main() {
-	resp,err := http.Get("http://www.zhenai.com/zhenghun")
-	if err != nil {
-		panic(err)
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		fmt.Println("Error : status code error ",resp.StatusCode)
-		return
-	}
-
-	all, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	//fmt.Printf("%s",all)
-	printCityList(all)
-}
-func printCityList(contents []byte) {
-	re:= regexp.MustCompile("<a href=\"(http://www.zhenai.com/zhenghun/[0-9a-z]+)\"[^>]*>([^<]+)</a>")
-	matches := re.FindAllSubmatch(contents,-1)
-	for _,m := range matches  {
-		fmt.Printf("City :%s URL:%s \n",m[2],m[1])
-		//for _,subMath := range m {
-		//	fmt.Printf("%s ",subMath)
-		//}
-		//fmt.Println()
-	}
-	fmt.Println("All city counts ",len(matches))
+	engine.Run(engine.Request{
+		Url:"http://www.zhenai.com/zhenghun",
+		ParserFunc:parser.ParseCityList,
+	})
 }
